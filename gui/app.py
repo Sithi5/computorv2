14a4bceafc7a15cb746277df31f7af3697f49589
Sithi5 <ma.sithis@gmail.com>
2021-10-14 17:39:35 +0200

    [feat]: Add matrice button in gui.

--- gui/app.py
+++ gui/app.py
@@ -34,7 +34,8 @@ class Application(Frame):
             for char in clearButton:
                 button(erase, LEFT, char, lambda storeObj=display, q=char: storeObj.set(""))
 
-        # Add fn(x) and = and ? buttons
+        # Add 'fn(x)' and '=' and '?' buttons
+        special_button_char = "=?"
         special_button = iCalc(self, TOP)
         button(
             special_button,
@@ -42,23 +43,29 @@ class Application(Frame):
             "f(x)",
             lambda storeObj=display, q="f(x)": storeObj.set(storeObj.get() + q),
         )
-        button(
-            special_button,
-            LEFT,
-            "=",
-            lambda storeObj=display, q="=": storeObj.set(storeObj.get() + q),
-        )
-        button(
-            special_button,
-            LEFT,
-            "?",
-            lambda storeObj=display, q="?": storeObj.set(storeObj.get() + q),
-        )
-        for numButton in ("iX()^%", "789/", "456*", "123-", "0.+"):
-            FunctionNum = iCalc(self, TOP)
-            for char in numButton:
+        for char in special_button_char:
+            button(
+                special_button,
+                LEFT,
+                char,
+                lambda storeObj=display, q=char: storeObj.set(storeObj.get() + q),
+            )
+
+        parenthesis_and_matrice_button = iCalc(self, TOP)
+        parenthesis_and_matrice_button_char = "()[],;"
+        for char in parenthesis_and_matrice_button_char:
+            button(
+                parenthesis_and_matrice_button,
+                LEFT,
+                char,
+                lambda storeObj=display, q=char: storeObj.set(storeObj.get() + q),
+            )
+
+        for numeric_button in ("iX^%", "789/", "456*", "123-", "0.+"):
+            function_button = iCalc(self, TOP)
+            for char in numeric_button:
                 button(
-                    FunctionNum,
+                    function_button,
                     LEFT,
                     char,
                     lambda storeObj=display, q=char: storeObj.set(storeObj.get() + q),
